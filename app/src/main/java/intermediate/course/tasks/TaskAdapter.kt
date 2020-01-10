2f5c324bdad15a7937a8dedf5927d2bea0b2f46f
bianca.sozzi <bianca.sozzi@klarna.com>
2020-01-10 11:17:35 +0100

    add notes recycler view

--- app/src/main/java/intermediate/course/tasks/TaskAdapter.kt
+++ app/src/main/java/intermediate/course/tasks/TaskAdapter.kt
@@ -24,7 +24,7 @@ class TaskAdapter(
 
     class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
         fun onBind(task: Task){
-            view.titleView.text = task.title
+            view.textView.text = task.title
         }
     }
 }
