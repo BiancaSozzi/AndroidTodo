779dc36d40a367ac598d97bc578da59fd0040c1f
bianca.sozzi <bianca.sozzi@klarna.com>
2020-01-27 09:02:04 +0100

    inflate menu + create fragments

--- app/src/main/java/intermediate/course/create/CreateActivity.kt
+++ app/src/main/java/intermediate/course/create/CreateActivity.kt
@@ -1,26 +1,33 @@
 package intermediate.course.create
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import intermediate.course.R
 import intermediate.course.navigation.NavigationActivity
 import kotlinx.android.synthetic.main.activity_create.*
 
-class CreateActivity : AppCompatActivity() {
+class CreateActivity : AppCompatActivity(), CreateNoteFragment.OnFragmentInteractionListener, CreateTaskFragment.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
+        supportActionBar?.title = ""
+
         intent.getStringExtra(NavigationActivity.FRAGMENT_TYPE_KEY).run {
-            create_activity_title.text = if (this == NavigationActivity.FRAGMENT_VALUE_TASk) {
-                "this is a task"
+            if (this == NavigationActivity.FRAGMENT_VALUE_TASk) {
+                createFragment(CreateTaskFragment.newInstance())
             } else if (this == NavigationActivity.FRAGMENT_VALUE_NOTE) {
-                "this is a note"
-            } else {
-                "something went wrong!"
+                createFragment(CreateNoteFragment.newInstance())
             }
         }
+
+
 
 //        create_activity_title.text = if (intent.getStringExtra(NavigationActivity.FRAGMENT_TYPE_KEY) == NavigationActivity.FRAGMENT_VALUE_TASk){
 //            "this is a task"
@@ -28,4 +35,24 @@ class CreateActivity : AppCompatActivity() {
 //            "this is a note"
 //        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_save, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.saveItem -> Toast.makeText(this, "Save Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun createFragment(fragment: Fragment) {
+         supportFragmentManager.beginTransaction().replace(R.id.fragmentHolder, fragment).commit()
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {}
 }
---
+++ app/src/main/java/intermediate/course/create/CreateTaskFragment.kt
@@ -0,0 +1,46 @@
+package intermediate.course.create
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import intermediate.course.R
+
+class CreateTaskFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_task, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        fun newInstance() = CreateTaskFragment()
+    }
+}
